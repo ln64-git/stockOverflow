a9ea9369652366384ca31248c0bbae26b666e745
Lucian <Lucian.io@outlook.com>
2023-09-29 01:49:49 -0400

    Cleaning up

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,39 +1,17 @@
 <script setup lang="ts">
-import {useTokenStore} from "../store/token-store"
-import axios, {AxiosResponse, AxiosError} from "axios"
-import {useRouter} from "vue-router"
+import {useTokenStore} from "~/store/token-store"
 
+let groupList: Group[] = await getGroupList()
 const tokenStore = useTokenStore()
 const router = useRouter()
-let groupList: Group[] = []
-let dataLoaded = false
-
-const fetchData = async () => {
-  if (!tokenStore.token) {
-    router.push({path: "/signin"})
-    return
-  }
-  const apiUrl = "http://localhost:9000/groups"
-  const headers = {
-    Authorization: `Bearer ${tokenStore.token}`,
-  }
-  try {
-    const response: AxiosResponse = await axios.get(apiUrl, {headers})
-    console.log(response)
-    groupList = response.data
-  } catch (error: any) {
-    console.error("Error:", error.message)
-  } finally {
-    dataLoaded = true
-  }
+if (!tokenStore.token) {
+  router.push({path: "/signin"})
 }
-await fetchData()
 </script>
 
 <template>
   <div>
     <ul
-      v-if="dataLoaded"
       class="card w-96 bg-base-100 shadow-xl"
       v-for="(group, index) in groupList"
       :key="index"
@@ -46,6 +24,5 @@ await fetchData()
         </div>
       </div>
     </ul>
-    <p v-else>Loading...</p>
   </div>
 </template>
