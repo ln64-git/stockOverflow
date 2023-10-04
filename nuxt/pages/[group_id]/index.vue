fe523c40545be760a4a539dc362d3ff3a4664460
Lucian <Lucian.io@outlook.com>
2023-10-04 09:11:36 -0400

    slug accessable

---
+++ nuxt/pages/[group_id]/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Group page</h1>
+  <p>Slug: {{ $route.params.group_id }}</p>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const groupId = route.params.groupId
+</script>
+
--- nuxt/pages/_groupId/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Group page</h1>
-</template>
